67a5dcd34313ea4cd4259f86ef2c0133417912d5
Vilde Almestad <vildealmestad@MacBook-Air-3.local>
2020-11-15 19:09:22 +0100

    hei

--- App.tsx
+++ App.tsx
@@ -12,7 +12,7 @@ const ipv4Adress = manifest.debuggerHost?.split(':')[0];
 
 export default function App() {
   const client = new ApolloClient({
-    uri: 'http://192.168.0.101:4000',    
+    uri: 'http://192.168.1.99:4000',    
     cache: new InMemoryCache()
   });
 //10.22.7.35
